fcf3d7a59c98d6c43c969422be1949c6d0158985
RJdeck <rockchen4@gmail.com>
2023-01-04 17:49:51 +0800

    fix: del ori config

--- meta2d/common/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from dotenv import dotenv_values
-_config = dotenv_values(".env")
-
-BUCKET_NAME = os.getenv("AWS_BUCKET_NAME", "image-bucket-123")
-TOKEN = os.getenv("AWS_TOKEN", "123")
-IMAGE_URL = os.getenv("IMAGE_URL", "http://localhost:5000")
-REGION = os.getenv("AWS_REGION", "ap-east-1")
